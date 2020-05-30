3ca028eb13461559c37161cd2110b3b5224dea3a
Silvia Almeida <silviaalmeida123@hotmail.com>
2020-05-30 23:48:33 +0100

    vue-smma: templating examples on 'About' page

---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="about-component">
+    <p>
+      <b>Prop 'msg' of the AboutComponent:</b>
+      {{ msg }}
+    </p>
+    <p>
+      <b>Prop 'number' of the AboutComponent:</b>
+      {{ number }}
+    </p>
+    <div class="buttons">
+      <button v-on:click="showMessage">v-on:click example</button>
+      <button @click="showMessage">@click example</button>
+    </div>
+    <span v-html="rawHtml"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutComponent",
+  props: {
+    msg: { type: String, default: "This is the default message." } // required: true
+  },
+  data() {
+    return {
+      rawHtml: '<p style="font-size:18px;color:red;">v-html is cool!</p>',
+      number: 10
+    };
+  },
+  methods: {
+    showMessage() {
+      alert("Hello!");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+p {
+  margin: 30px 0;
+}
+button {
+  margin: 10px 0;
+}
+
+.about-component {
+  border: 1px solid black;
+  display: inline-block;
+  padding: 15px;
+}
+
+.buttons {
+  display: inline-grid;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h3>{{ title }}</h3>
+    <!-- we can use v-if or v-show to control the element's visibilty -->
+    <AboutComponent v-if="isComponentVisible" :msg="'Hey!'"></AboutComponent>
   </div>
 </template>
+
+<script>
+import AboutComponent from "@/components/AboutComponent.vue";
+export default {
+  name: "About",
+  components: { AboutComponent: AboutComponent },
+  data() {
+    return {
+      title: "This is the title of About.vue!",
+      isComponentVisible: true
+    };
+  }
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
